19f17eb394ee5e0515bb3343f9f56a5b00ccc071
ZxBing0066 <ZxBing0066@gmail.com>
2018-12-03 18:26:37 +0800

    feat(Checkbox): add support of theme

--- src/components/Checkbox/style/index.js
+++ src/components/Checkbox/style/index.js
@@ -1,17 +1,30 @@
 import styled, { css } from 'styled-components';
 
-import Icon from 'components/Icon';
-import { inlineBlockWithVerticalMixin, Color, Height } from 'src/style';
+import Icon from 'src/components/Icon';
+import { inlineBlockWithVerticalMixin } from 'src/style';
+import defaultTheme from 'src/components/ThemeProvider/theme';
 
-const disabledMixin = css`
-    color: ${Color.font.disabled};
-    cursor: not-allowed;
-    pointer-events: none;
+export const CheckboxIcon = styled(Icon)`
+    margin-right: 8px;
+    font-size: 14px;
 `;
 
-const sizeMixin = ({ size }) => css`
+const propsMixin = ({ theme: { Height, colorMap, fontSize }, size, disabled, checked }) => css`
     height: ${Height[size]};
     line-height: ${Height[size]};
+    font-size: ${fontSize};
+    color: ${colorMap.default.text};
+
+    ${disabled &&
+        css`
+            color: ${colorMap.disabled.text};
+            cursor: not-allowed;
+            pointer-events: none;
+        `};
+
+    ${/*sc-sel */ CheckboxIcon} {
+        color: ${disabled ? colorMap.disabled.icon : checked ? colorMap.active.icon : colorMap.default.icon};
+    }
 `;
 
 export const CheckboxWrap = styled.span`
@@ -19,15 +32,11 @@ export const CheckboxWrap = styled.span`
     position: relative;
 
     ${inlineBlockWithVerticalMixin};
-    ${sizeMixin};
-    ${({ disabled }) => disabled && disabledMixin};
+    ${propsMixin};
 `;
-
-export const CheckboxIcon = styled(Icon)`
-    margin-right: 8px;
-
-    color: ${props => (props.disabled ? Color.font.disabled : props.checked ? Color.font.blue : Color.font.default)};
-`;
+CheckboxWrap.defaultProps = {
+    theme: defaultTheme
+};
 
 export const CheckboxGroupWrap = styled.div`
     ${/* sc-sel */ CheckboxWrap} {
