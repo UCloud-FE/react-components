b27bcd135eca5c36632e9d108110c2828de46472
ZxBing0066 <ZxBing0066@gmail.com>
2018-12-25 17:35:09 +0800

    docs(StyleGuide): add theme changer on top

---
+++ src/components/ThemeProvider/ocean.js
@@ -0,0 +1,12 @@
+export default {
+    colorList: {
+        primary: '#415bf5',
+        primary1: '#415bf5',
+        primary2: '#415bf5',
+        primary3: '#f0f2ff',
+        primary4: '#fafaff',
+        primary5: '#c3caf7',
+        primary6: '#fcfcfd',
+        primary7: '#cae3ff'
+    }
+};
