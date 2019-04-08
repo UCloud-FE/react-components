57cfcae8d4bbffa5990f7e715bdee73012080e97
ZxBing0066 <ZxBing0066@gmail.com>
2019-04-08 17:34:27 +0800

    feat(Form): add Group of form

--- src/components/Form/style/index.js
+++ src/components/Form/style/index.js
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Row, Col } from 'src/components/Grid';
 import config from 'src/config';
+import addDefaultThemeProps from 'src/components/ThemeProvider/addDefaultThemeProps';
 
 const { prefixCls: _prefixCls } = config;
 const prefixCls = _prefixCls + '-form';
@@ -34,3 +35,29 @@ export const ControllerWrap = styled(Col).attrs({
 })`
     /* empty */
 `;
+
+export const GroupWrap = styled.div.attrs({
+    className: prefixCls + '-group'
+})`
+    & + & {
+        margin-top: 24px;
+    }
+    /* empty */
+`;
+
+export const GroupTitle = styled.div.attrs({
+    className: prefixCls + '-group-title'
+})`
+    font-weight: bold;
+    line-height: 22px;
+    padding: 12px 0;
+    margin-bottom: 24px;
+
+    ${({ theme: { colorList, colorMap, titleFontSize } }) => css`
+        font-size: ${titleFontSize};
+        border-bottom: 1px solid ${colorMap.default.border};
+        color: ${colorList.title};
+    `};
+`;
+
+addDefaultThemeProps(GroupTitle);
