191a7ca4ae22c343236de6c4480d853cb29de3df
ZxBing0066 <ZxBing0066@gmail.com>
2019-04-16 15:17:55 +0800

    feat(Icon): reset icon style
    close #118

--- src/components/Icon/style/index.js
+++ src/components/Icon/style/index.js
@@ -15,6 +15,7 @@ const iconSpinMixin = css`
 export const IconWrap = styled.i.attrs({
     className: ({ spin, type }) => classnames(prefixCls, `icon__${type}`, spin && `${prefixCls}-spin`)
 })`
+    vertical-align: baseline;
     &&& {
         ${({ spin }) => spin && iconSpinMixin};
     }
