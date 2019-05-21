3f2b285b883ca333d3d76cb02e6fbaeeb1649d35
ZxBing0066 <ZxBing0066@gmail.com>
2019-05-21 11:16:09 +0800

    fix(Tooltip): fix tooltip auto align style

--- src/components/Tooltip/style/index.js
+++ src/components/Tooltip/style/index.js
@@ -1,11 +1,17 @@
-import styled, { css } from 'styled-components';
-import _ from 'lodash';
+import styled, { css, injectGlobal } from 'styled-components';
 
+import { prefixCls as popoverPrefixCls } from 'src/components/Popover/style';
 import addDefaultThemeProps from 'src/components/ThemeProvider/addDefaultThemeProps';
+import config from 'config';
+
+const { prefixCls: _prefixCls } = config;
+export const prefixCls = _prefixCls + '-tooltip';
 
 /* stylelint-disable property-no-unknown,no-duplicate-selectors */
 const arrowWidth = '6px';
 const borderWidth = '1px';
+
+export const tooltipPopupClassName = prefixCls + '-popup';
 
 export const ContentWrap = styled.div(({ theme: { Tooltip: tooltipTheme }, themeType }) => {
     tooltipTheme = tooltipTheme[themeType];
@@ -38,79 +44,125 @@ export const ArrowInner = styled.span`
     ${arrowMixin};
 `;
 
-const Position = ['top', 'right', 'bottom', 'left'];
-
-const offsetPosition = (position, offset) => {
-    const index = _.findIndex(Position, p => p === position);
-    return Position[(index + offset) % 4];
-};
-
-export const TooltipWrap = styled.div(({ theme: { Tooltip: tooltipTheme }, themeType, placement }) => {
-    tooltipTheme = tooltipTheme[themeType];
-    let positions = placement.match(/([a-z]+)([A-Za-z]*)/);
-    return css`
-        ${() => {
-            if (!positions) return;
-            let position = positions[1];
-
-            return css`
+injectGlobal`
+    .${tooltipPopupClassName} {
+        &.${popoverPrefixCls}-placement-bottom,
+        &.${popoverPrefixCls}-placement-bottomLeft,
+        &.${popoverPrefixCls}-placement-bottomRight {
+            ${Arrow}, ${ArrowInner} {
+                margin-left: -${arrowWidth};
+                border-width: 0 ${arrowWidth} ${arrowWidth} ${arrowWidth};
+                border-top-color: transparent;
+                border-left-color: transparent;
+                border-right-color: transparent;
+            }
             ${Arrow} {
-                ${offsetPosition(position, 2)}: -${arrowWidth};
-                margin-${offsetPosition({ bottom: 'top', left: 'right' }[position] || position, 3)}: -${arrowWidth};
-                border-${position}-width: ${arrowWidth};
-                border-${offsetPosition(position, 1)}-width: ${arrowWidth};
-                border-${offsetPosition(position, 3)}-width: ${arrowWidth};
-
-                border-${position}-color: ${tooltipTheme.arrow.border};
+                top: -${arrowWidth};
             }
-
             ${ArrowInner} {
-                ${offsetPosition(position, 2)}: ${borderWidth};
-                margin-${offsetPosition({ bottom: 'top', left: 'right' }[position] || position, 3)}: -${arrowWidth};
-                border-${position}-width: ${arrowWidth};
-                border-${offsetPosition(position, 1)}-width: ${arrowWidth};
-                border-${offsetPosition(position, 3)}-width: ${arrowWidth};
-
-                border-${position}-color: ${tooltipTheme.arrow.background};
+                top: ${borderWidth};
             }
-        `;
-        }};
+        }
+        &.${popoverPrefixCls}-placement-top,
+        &.${popoverPrefixCls}-placement-topLeft,
+        &.${popoverPrefixCls}-placement-topRight {
+            ${Arrow}, ${ArrowInner} {
+                margin-left: -${arrowWidth};
+                border-width: ${arrowWidth} ${arrowWidth} 0 ${arrowWidth};
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+                border-right-color: transparent;
+            }
+            ${Arrow} {
+                bottom: -${arrowWidth};
+            }
+            ${ArrowInner} {
+                bottom: ${borderWidth};
+            }
+        }
+        &.${popoverPrefixCls}-placement-left,
+        &.${popoverPrefixCls}-placement-leftTop,
+        &.${popoverPrefixCls}-placement-leftBottom {
+            ${Arrow}, ${ArrowInner} {
+                margin-top: -${arrowWidth};
+                border-width: ${arrowWidth} 0 ${arrowWidth} ${arrowWidth};
+                border-top-color: transparent;
+                border-bottom-color: transparent;
+                border-right-color: transparent;
+            }
+            ${Arrow} {
+                right: -${arrowWidth};
+            }
+            ${ArrowInner} {
+                right: ${borderWidth};
+            }
+        }
+        &.${popoverPrefixCls}-placement-right,
+        &.${popoverPrefixCls}-placement-rightTop,
+        &.${popoverPrefixCls}-placement-rightBottom {
+            ${Arrow}, ${ArrowInner} {
+                margin-top: -${arrowWidth};
+                border-width: ${arrowWidth} ${arrowWidth} ${arrowWidth} 0;
+                border-top-color: transparent;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+            }
+            ${Arrow} {
+                left: -${arrowWidth};
+            }
+            ${ArrowInner} {
+                left: ${borderWidth};
+            }
+        }
+        &.${popoverPrefixCls}-placement-bottomLeft,
+        &.${popoverPrefixCls}-placement-topLeft {
+            ${Arrow} {
+                left: 15%;
+            }
+        }
+        &.${popoverPrefixCls}-placement-bottomRight,
+        &.${popoverPrefixCls}-placement-topRight {
+            ${Arrow} {
+                right: 15%;
+            }
+        }
+        &.${popoverPrefixCls}-placement-leftTop,
+        &.${popoverPrefixCls}-placement-rightTop {
+            ${Arrow} {
+                top: 15%;
+                margin-top: 0;
+            }
+        }
+        &.${popoverPrefixCls}-placement-leftBottom,
+        &.${popoverPrefixCls}-placement-rightBottom {
+            ${Arrow} {
+                bottom: 15%;
+            }
+        }
+        &.${popoverPrefixCls}-placement-top,
+        &.${popoverPrefixCls}-placement-bottom {
+            ${Arrow} {
+                left: 50%;
+            }
+        }
+        &.${popoverPrefixCls}-placement-left,
+        &.${popoverPrefixCls}-placement-right {
+            ${Arrow} {
+                top: 50%;
+            }
+        }
+    }
+`;
 
-        ${() => {
-            if (!positions || positions[2]) return;
-            let position = positions[1];
-
-            return css`
-                ${Arrow} {
-                    ${offsetPosition({ bottom: 'top', left: 'right' }[position] || position, 3)}: 50%;
-                }
-            `;
-        }};
-        ${/Left$/.test(placement) &&
-            css`
-                ${Arrow} {
-                    left: 15%;
-                }
-            `};
-        ${/Right$/.test(placement) &&
-            css`
-                ${Arrow} {
-                    right: 15%;
-                }
-            `};
-        ${/Top$/.test(placement) &&
-            css`
-                ${Arrow} {
-                    top: 15%;
-                    margin-top: 0;
-                }
-            `};
-        ${/Bottom$/.test(placement) &&
-            css`
-                ${Arrow} {
-                    bottom: 15%;
-                }
-            `};
+export const TooltipWrap = styled.div(({ theme: { Tooltip: tooltipTheme }, themeType }) => {
+    tooltipTheme = tooltipTheme[themeType];
+    return css`
+        ${Arrow} {
+            border-color: ${tooltipTheme.arrow.border};
+        }
+        ${ArrowInner} {
+            border-color: ${tooltipTheme.arrow.background};
+        }
     `;
 });
 addDefaultThemeProps(ContentWrap, TooltipWrap);
