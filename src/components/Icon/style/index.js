aafd28620e5aed2f17e713d77d6de43915fa54b1
ZxBing0066 <ZxBing0066@gmail.com>
2019-11-27 15:20:08 +0800

    fix(Icon): clean icon props to html
    close #185

--- src/components/Icon/style/index.js
+++ src/components/Icon/style/index.js
@@ -1,20 +1,17 @@
 import styled, { css } from 'styled-components';
-import { spinMixin } from 'style';
-import classnames from 'classnames';
 
+import { spinMixin } from 'src/style';
 import config from 'src/config';
 
 const { prefixCls: _prefixCls } = config;
-const prefixCls = _prefixCls + '-icon';
+export const prefixCls = _prefixCls + '-icon';
 
 const iconSpinMixin = css`
     ${spinMixin};
     line-height: normal;
 `;
 
-export const IconWrap = styled.i.attrs({
-    className: ({ spin, type }) => classnames(prefixCls, `icon__${type}`, spin && `${prefixCls}-spin`)
-})`
+export const IconWrap = styled.i`
     vertical-align: baseline;
     &&& {
         ${({ spin }) => spin && iconSpinMixin};
