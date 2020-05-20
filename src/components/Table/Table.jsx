2b762022c9e3ff6185998a5b13142326ceecc241
ZxBing0066 <ZxBing0066@gmail.com>
2020-05-20 18:59:12 +0800

    * feat(Table): remove columns check
    close #222

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -269,9 +269,6 @@ class Table extends Component {
     };
     check = props => {
         const { columns } = props;
-        if (_.isEmpty(columns)) {
-            return emptyColumnsWarn();
-        }
         _.each(columns, column => {
             if (column.key === undefined) missingColumnKeyWarn();
         });
