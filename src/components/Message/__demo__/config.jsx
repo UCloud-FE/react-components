d41fe4ffe3cddaffbef1c589452f43a1d9ed5288
ZxBing0066 <ZxBing0066@gmail.com>
2020-05-22 18:13:32 +0800

    feat(Message): add config
    close #223

---
+++ src/components/Message/__demo__/config.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Message from 'components/Message';
+import Button from 'components/Button';
+
+// demo start
+const Demo = () => {
+    return (
+        <div>
+            <Button
+                onClick={() =>
+                    Message.config({
+                        duration: 10000000,
+                        top: 200
+                    })
+                }
+            >
+                change duration and top
+            </Button>
+            <Button
+                onClick={() =>
+                    Message.config({
+                        duration: null,
+                        top: 0
+                    })
+                }
+            >
+                change duration to null
+            </Button>
+            <Button
+                onClick={() =>
+                    Message.config({
+                        duration: 3000,
+                        top: 20
+                    })
+                }
+            >
+                reset config
+            </Button>
+            {['message', 'warning', 'success', 'error'].map(type => (
+                <div className="demo-wrap" key={type}>
+                    <Button
+                        style={{ width: 150 }}
+                        onClick={() =>
+                            Message[type](<div>this is a message</div>, undefined, () => console.log('onClose'))
+                        }
+                    >
+                        {type}
+                    </Button>
+                </div>
+            ))}
+        </div>
+    );
+};
+// demo end
+
+export default Demo;
