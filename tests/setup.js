e6e22640b87551bdaea422262e5efbdbd3f9e2ff
ZxBing0066 <ZxBing0066@gmail.com>
2020-08-26 17:02:40 +0800

    feat(project): upgrade react to 16

--- tests/setup.js
+++ tests/setup.js
@@ -5,14 +5,12 @@ global.requestAnimationFrame =
     };
 
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-15');
+const Adapter = require('enzyme-adapter-react-16');
 const moment = require('moment-timezone');
 
 moment.tz.setDefault('Asia/Shanghai');
 import 'moment/locale/zh-cn';
-moment()
-    .locale('zh-cn')
-    .utcOffset(8);
+moment().locale('zh-cn').utcOffset(8);
 
 Enzyme.configure({ adapter: new Adapter() });
 
