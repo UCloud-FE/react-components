4333860754a226f087c11ce1fa66dba9c32ee1b6
Jace Yi <yijinchun@outlook.com>
2020-09-18 11:32:16 +0800

    test(test): add test for Tabs and update Radio test
    close #254

--- src/components/Radio/__tests__/index.test.js
+++ src/components/Radio/__tests__/index.test.js
@@ -43,32 +43,33 @@ describe('Radio', () => {
     });
 
     test('check controlled Group', async () => {
-        const onChange = jest.fn();
+        let radioValue = 2;
+        const onChange = jest.fn(value => (radioValue = value));
         const wrapper = mount(
-            <Group onChange={onChange}>
+            <Group value={radioValue} onChange={onChange}>
                 <Radio value={1}>1</Radio>
                 <Radio value={2}>1</Radio>
             </Group>
         );
         const children = wrapper.find('span.uc-fe-radio');
         expect(children).toHaveLength(2);
+        expect(children.at(1).hasClass('uc-fe-radio-checked')).toBe(true);
+
         children.at(0).simulate('click');
+        // click 后有 防抖 所以做一个延迟
         await new Promise(resolve => {
-            setTimeout(() => {
-                expect(onChange).toHaveBeenCalledTimes(1);
-                expect(onChange).toHaveBeenLastCalledWith(1);
-                resolve();
-            }, 10);
+            setTimeout(resolve, 10);
         });
+        expect(onChange).toHaveBeenCalledTimes(1);
+        expect(onChange).toHaveBeenLastCalledWith(1);
 
-        wrapper.setProps({ disabled: true });
+        wrapper.setProps({ disabled: true, value: radioValue });
+        expect(wrapper.find('span.uc-fe-radio').at(0).hasClass('uc-fe-radio-checked')).toBe(true);
+
         children.at(1).simulate('click');
-
         await new Promise(resolve => {
-            setTimeout(() => {
-                expect(onChange).toHaveBeenCalledTimes(1);
-                resolve();
-            }, 10);
+            setTimeout(resolve, 10);
         });
+        expect(onChange).toHaveBeenCalledTimes(1);
     });
 });
