9385b97af80e5dab89ba8ebc0168255df0b6886c
ZxBing0066 <ZxBing0066@gmail.com>
2021-01-11 12:59:08 +0800

    fix(Modal): fix modal ts not completely bug

--- src/components/Modal/Content.jsx
+++ src/components/Modal/Content.jsx
@@ -1,16 +1,11 @@
-import React, { HTMLAttributes, useCallback, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { InheritProvider } from 'src/components/Popover/ContainerContext';
 
 import { SContent } from './style';
 
-type ContentProps = {
-    /** 定义容器最大高度，传入后超过高度会出滚动 */
-    maxHeight?: string;
-} & HTMLAttributes<HTMLDivElement>;
-
-const Content = ({ ...rest }: ContentProps) => {
+const Content = ({ ...props }) => {
     const containerRef = useRef(null);
     const getContainer = useCallback(() => {
         return containerRef.current;
@@ -19,12 +14,14 @@ const Content = ({ ...rest }: ContentProps) => {
         <InheritProvider value={{ getPopupContainer: getContainer }}>
             <>
                 <div ref={containerRef}></div>
-                <SContent {...rest} />
+                <SContent {...props} />
             </>
         </InheritProvider>
     );
 };
 Content.propTypes = {
+    /** 定义容器最大高度，传入后超过高度会出滚动 */
+    maxHeight: PropTypes.string,
     /** @ignore */
     className: PropTypes.string
 };
