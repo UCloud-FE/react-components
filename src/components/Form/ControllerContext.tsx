f94faa43746c9acf486f7699064f0103cc284b3e
ZxBing0066 <ZxBing0066@gmail.com>
2021-06-08 09:55:30 +0800

    fix(Input): fix input status interface

--- src/components/Form/ControllerContext.tsx
+++ src/components/Form/ControllerContext.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const ControllerContext = React.createContext<{ status?: string }>({});
+const ControllerContext = React.createContext<{ status?: 'error' }>({});
 
 export default ControllerContext;
