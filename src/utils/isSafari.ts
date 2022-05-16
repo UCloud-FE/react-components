aa2b1f0eda7d07b5a71b7f8fe0a57fbecf2fa1fb
ZxBing0066 <ZxBing0066@gmail.com>
2022-05-16 10:18:33 +0800

    fix(All): fix isSafari in chrome

--- src/utils/isSafari.ts
+++ src/utils/isSafari.ts
@@ -1,3 +1,3 @@
-const isSafari = navigator.userAgent.toLowerCase().indexOf('safari/') > -1;
-
+const isChrome = navigator.userAgent.toLowerCase().indexOf('chrome/') > -1;
+const isSafari = !isChrome && navigator.userAgent.toLowerCase().indexOf('safari/') > -1;
 export default isSafari;
