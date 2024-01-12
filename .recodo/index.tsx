dbccf32d1aabdc4b4155e28de3a40a9d76ded675
WangLin <lin.wang@ucloud.cn>
2024-01-12 18:46:52 +0800

    chore(.recode): update design-token-editor version to 0.3.8

--- .recodo/index.tsx
+++ .recodo/index.tsx
@@ -46,12 +46,12 @@ mod.config({
         },
         '@ucloud-fe/mod': { js: [] },
         'design-token-editor': {
-            js: 'https://cdn.jsdelivr.net/npm/@ucloud-fe/design-token-editor@0.3.7/dist/design-token-editor.umd.js',
+            js: 'https://cdn.jsdelivr.net/npm/@ucloud-fe/design-token-editor@0.3.8/dist/design-token-editor.umd.js',
             type: 'amd',
             dep: ['react', 'design-token-editor/style']
         },
         'design-token-editor/style': {
-            css: 'https://cdn.jsdelivr.net/npm/@ucloud-fe/design-token-editor@0.3.7/dist/style.css',
+            css: 'https://cdn.jsdelivr.net/npm/@ucloud-fe/design-token-editor@0.3.8/dist/style.css',
             type: 'immediate'
         },
         'design-token-file': {
