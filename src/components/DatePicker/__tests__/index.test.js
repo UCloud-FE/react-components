306040f7f2f918ba96530762878e563a120d9e6b
jamie.liu <jamie.liu@ucloud.cn>
2024-04-09 19:04:19 +0800

    test(DatePicker): add tests to DatePicker
    add tests to DatePicker

--- src/components/DatePicker/__tests__/index.test.js
+++ src/components/DatePicker/__tests__/index.test.js
@@ -23,7 +23,6 @@ describe('DatePicker', () => {
         }
         const RangeComp = mount(<Demo />);
         const startTimeDom = RangeComp.find('.uc-fe-datepicker-range span').at(0).childAt(0).find('input');
-        const endTimeDom = RangeComp.find('.uc-fe-datepicker-range span').at(0).childAt(1).find('input');
         const confirmBtnDom = () => RangeComp.find('.uc-fe-datepicker-footer .uc-fe-button').at(0);
         startTimeDom.simulate('click'); // click start time
 
@@ -46,24 +45,18 @@ describe('DatePicker', () => {
         confirmBtnDom().simulate('click'); // click the confirm button
         expect(RangeComp.find('.uc-fe-datepicker-footer > span').at(0).html()).toBe('<span></span>');
 
-        startTimeDom.simulate('click'); // click start time
-        RangeComp.find('.uc-fe-calendar-tbody > .uc-fe-calendar-row')
-            .at(0)
-            .find('.uc-fe-calendar-cell')
-            .at(4)
-            .simulate('click'); // click April 1st
-        confirmBtnDom().simulate('click'); // click the confirm button
-        expect(RangeComp.find('.uc-fe-datepicker-footer > .uc-fe-datepicker-tip-error > span').at(0).html()).toBe(
-            '<span>超出最大范围</span>'
-        );
-        confirmBtnDom().simulate('click'); // click the confirm button
+        const endTimeDom = RangeComp.find('.uc-fe-datepicker-range span').at(0).childAt(1).find('input');
         endTimeDom.simulate('click'); // click end time
-        RangeComp.find('.uc-fe-calendar-tbody > .uc-fe-calendar-row')
-            .at(0)
+        RangeComp.find('.uc-fe-calendar-tbody')
+            .at(1)
+            .find('.uc-fe-calendar-row')
+            .at(1)
             .find('.uc-fe-calendar-cell')
-            .at(7)
-            .simulate('click'); // click April 2nd
-        confirmBtnDom().simulate('click'); // click the confirm button
-        expect(RangeComp.find('.uc-fe-datepicker-footer > span').at(0).html()).toBe('<span></span>');
+            .at(1)
+            .simulate('click'); // click April 7th
+        RangeComp.find('.uc-fe-datepicker-footer').at(9).find('.uc-fe-button').at(0).simulate('click');
+        expect(RangeComp.find('.uc-fe-datepicker-footer > .uc-fe-datepicker-tip-error > span').at(0).html()).toBe(
+            '<span>不能为空</span>'
+        );
     });
 });
